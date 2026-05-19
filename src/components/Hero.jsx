3bfc63e17c8804460de113b2bfcbb146fe706678
Divan27 <dilanzam8@gmail.com>
2026-05-19 00:46:27 -0600

    Uso de colores

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <div className="hero-content">
+        <h1>Feel The Entertainment</h1>
+
+        <p>
+          Música y películas en una experiencia moderna.
+        </p>
+
+        <button>Explore Now</button>
+      </div>
+    </section>
+  );
+}
+
+export default Hero;
